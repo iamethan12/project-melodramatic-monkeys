0daafc92484bd477499a4e99c616798c1bd38333
iamethan12 <105604437+iamethan12@users.noreply.github.com>
2022-06-29 23:56:39 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+ 
+   print(mydb)
+
 
 isKayla = True
 
