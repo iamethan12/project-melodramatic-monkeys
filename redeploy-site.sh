908b6de2b528022e1764dd6262f1a7813eb0d1f6
iamethan12 <105604437+iamethan12@users.noreply.github.com>
2022-07-09 15:37:23 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-cd /root/project-kds_danieldiaz
+cd portfolio
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart myportfolio
-
-#session="portfolio_ddl29"
-#tmux new-session -d -s $session -c /root/project-kds_danieldiaz
-#window=0
-#tmux rename-window -t $session:$window 'portddl29'
-#tmux send-keys -t $session:$window 'git fetch && git reset origin/main --hard' C-m
-#tmux send-keys -t $session:$window 'python -m venv python3-virtualenv' C-m
-#tmux send-keys -t $session:$window 'source python3-virtualenv/bin/activate' C-m
-#tmux send-keys -t $session:$window 'pip install -r requirements.txt' C-m
-#tmux send-keys -t $session:$window 'export FLASK_ENV=development' C-m
-#tmux send-keys -t $session:$window 'flask run --host=0.0.0.0' C-m
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
